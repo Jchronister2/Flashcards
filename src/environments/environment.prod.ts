75a2092cd1a7f358c56b18ccaf18399b00ef6cdc
Jonathan Chronister <JonnyChronister@gmail.com>
2026-07-21 13:08:09 -0800

    Remove exposed Google API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,3 @@
 export const environment = {
-    production: true,
-    googleClientId: '249508522283-12oe76736pr2iknoefl5rd8li61dfbec.apps.googleusercontent.com',
-    googleApiKey: 'AIzaSyBWu0Vsv8xo0Jx2X6snOEzoBo4uSjA5LtI',
-    googleDiscoveryDocs: 'https://sheets.googleapis.com/$discovery/rest?version=v4',
-    googleScopes: 'https://www.googleapis.com/auth/drive.file'
+    production: true
 };
